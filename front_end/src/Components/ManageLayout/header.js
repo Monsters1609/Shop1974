994dc824842168545b17ce11a80d207372616653
Monster <ducdat169@gmail.com>
2023-03-27 08:56:42 +0700

    done header Manage

--- front_end/src/Components/ManageLayout/header.js
+++ front_end/src/Components/ManageLayout/header.js
@@ -9,17 +9,22 @@ function HeaderManager ({show}) {
             <div className={clsx(style.HeaderLeft_Path)}>
                 <h4 className={clsx(style.TitlePage)}>
                     <span className={clsx(style.IconHeart)}><i className="bi bi-heart-fill"></i></span>
-                    Manage{location.pathname}
+                    {/* kiểm tra path nếu pathname === /manage thì trả ra chuỗi rỗng còn ngược lại thì trả ra pathname */}
+                    Manage{location.pathname==='/manage'?'':location.pathname}
                     <span className={clsx(style.IconHeart)}><i className="bi bi-heart-fill"></i></span>
                 </h4>
             </div>
             <div className={clsx(style.HeaderRight)}>
                 <ul className={clsx(style.HeaderList)}>
                     <li className={clsx(style.HeaderItem)}>
-                        <Link to='/Notification'></Link>
+                        <Link className={clsx(style.HeaderLink)} to='/Notification'>
+                            <i className="bi bi-bell"></i>
+                        </Link>
                     </li>
                     <li className={clsx(style.HeaderItem)}>
-                        <Link to='/Account'></Link>
+                        <Link className={clsx(style.HeaderLink)} to='/Account'>
+                            <i className="bi bi-person-circle"></i>
+                        </Link>
                     </li>
                 </ul>
             </div>
